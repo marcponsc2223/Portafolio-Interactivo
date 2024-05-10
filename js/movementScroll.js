2791a73bac0ab4f440205faaa90763334c3959a7
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-05-10 18:55:14 +0200

    primera parte casi acabada
    primera parte casi acabada

--- js/movementScroll.js
+++ js/movementScroll.js
@@ -4,18 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     container.addEventListener("wheel", function(event) {
         var scrollAmount = event.deltaY;
         container.scrollLeft += scrollAmount;
-        if (event.deltaY >= 100) {
-            for (let i = 0; i < cloudsArray.length; i++) {
-                cloudsPositionX[i] += 50
+        // if (event.deltaY >= 100) {
+        //     for (let i = 0; i < cloudsArray.length; i++) {
+        //         cloudsPositionX[i] += 50
 
-            }
-        } else  {
-            if (container.scrollLeft > 0) {
-                for (let i = 0; i < cloudsArray.length; i++) {
-                    cloudsPositionX[i] -= 50
-                } 
-            }
-        }
+        //     }
+        // } else {
+        //      if (container.scrollLeft > 0) {
+        //     for (let i = 0; i < cloudsArray.length; i++) {
+        //         console.log(cloudsPositionX.lastIndexOf);
+        //             cloudsPositionX[i] -= 50  
+        //     } 
+        //      }
+        // }
         // Evita el desplazamiento vertical de la página
         event.preventDefault();
     });
