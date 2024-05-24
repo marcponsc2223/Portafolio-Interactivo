e0b2ba51cba8907c3265c12fecb28dd54afe3086
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-05-24 12:36:23 +0200

    portfolio acabado
    portfolio acabado

--- js/movementScroll.js
+++ js/movementScroll.js
@@ -1,9 +1,15 @@
+let character = document.getElementById('character')
+let formContact = document.getElementById('contactForm')
 document.addEventListener("DOMContentLoaded", function() {
     let container = document.querySelector('.container')
     var initialCloudsPositionX = []
     var isScrollingLeft = false 
+    
+    character.style.left = (window.innerWidth / 2 )+ 'px'
+    // console.log(formContact.offsetLeft);
     // let scrollTop = false
     // Registra la posición inicial de las nubes
+
     cloudsPositionX.forEach(function(x) {
         initialCloudsPositionX.push(x);
     });
@@ -11,7 +17,9 @@ document.addEventListener("DOMContentLoaded", function() {
     container.addEventListener("wheel", function(event) {
         var scrollAmount = event.deltaY;
         container.scrollLeft += scrollAmount;
-
+        character.left = container.scrollLeft
+        console.log(character.left + character.offsetWidth);
+        collisionCharacterWithForm()
         // Verificar si estamos al principio de la página y hacemos scroll hacia la izquierda
         if (container.scrollLeft <= 0 && scrollAmount < 0) {
             cloudsPositionX.forEach(function(x, index) {
@@ -54,3 +62,22 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+function collisionCharacterWithForm() {
+        if (
+            character.left + character.offsetWidth + (200)>= formContact.offsetLeft &&
+            character.left <= formContact.offsetLeft + formContact.offsetWidth &&
+            character.offsetTop + character.offsetHeight >= formContact.offsetTop &&
+            character.offsetTop <= formContact.offsetTop + formContact.offsetHeight
+        ) {
+            console.log('animationStart');
+            formContact.style.animation = 'fall 1.5s ease-in-out forwards'
+            formContact.addEventListener('animationend', function() {
+                document.body.classList.add('shake');
+                setTimeout(function() {
+                  document.body.classList.remove('shake');
+                }, 500);
+              })
+            
+        }
+}
